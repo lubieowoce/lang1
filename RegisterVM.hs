ade36c5b40ff9a4925915fa48a92fd4f7feeff1a
lubieowoce <lolzatu2@gmail.com>
2019-06-28 20:47:19 +0200

    Added a register-based VM

---
+++ RegisterVM.hs
@@ -0,0 +1,542 @@
+{-# LANGUAGE DeriveFunctor #-}
+
+module RegisterVM where
+
+import StackVM (Running (..), Running', VMRequest (..), setAt, intToBool, boolToInt)
+
+import Data.Map (Map)
+import qualified Data.Map as M
+
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+
+import Data.Char (chr, ord)
+
+import Data.Coerce
+import Debug.Trace (trace)
+
+import Control.Monad (forM, forM_)
+import Data.Maybe (catMaybes)
+import Data.List (intersperse)
+
+import Data.Bifunctor (first, second)
+import Data.List (mapAccumL)
+
+
+newtype InstructionIx     = InstructionIx Int     deriving (Eq, Show)
+newtype InstructionOffset = InstructionOffset Int deriving (Eq, Show)
+
+
+
+data Register
+    = SpecialRegister SpecialRegisterId
+    | GeneralRegister GeneralRegisterId
+    deriving (Eq)
+
+instance Show Register where
+    show (SpecialRegister r) = case r of 
+                                ProgramCounter -> "PC"
+                                StackBase      -> "EBP"
+                                StackTop       -> "ESP"
+    show (GeneralRegister (GeneralRegisterId i)) = "R"++(show i)
+
+data SpecialRegisterId
+    = ProgramCounter
+    | StackBase
+    | StackTop
+    deriving (Eq, Show)
+
+data SpecialRegisters = SpecialRegisters {
+        programCounter :: InstructionIx,
+        stackBase      :: Int,
+        stackTop       :: Int
+    } deriving (Eq, Show)
+
+
+newtype GeneralRegisterId = GeneralRegisterId Int deriving (Eq, Show)
+
+type GeneralRegisters = [IntVal]
+
+
+type IntVal = Int
+
+
+data Ref = Val | Ref Int deriving (Eq, Show)
+
+-- data Address
+--     = StackAddress Int
+--     | HeapAddress  Int
+--     deriving (Eq, Show)
+
+
+data OpVal
+    = OpValReg   Ref Register
+    | OpValConst Ref IntVal
+    deriving (Eq)
+
+instance Show OpVal where
+    show (OpValReg Val r) = show r
+    show (OpValConst Val r) = show r
+    show (OpValReg (Ref off) r)   = showRef r off
+    show (OpValConst (Ref off) n) = showRef n off
+
+showRef :: (Show a) => a -> Int -> String
+showRef a off = "[" ++ (show a) ++ off' ++ "]"
+    where off' = case compare off 0 of
+                    LT -> show off
+                    EQ -> ""
+                    GT -> "+" ++ (show off)
+
+data OpLoc
+    = OpLocReg  Ref Register
+    | OpLocAddr     IntVal
+    deriving (Eq)
+
+instance Show OpLoc where
+    show (OpLocReg Val r) = show r
+    show (OpLocReg (Ref off) r)   = showRef r off
+    show (OpLocAddr n) = showRef n 0
+
+
+data Op
+    = Nop
+    | Mov OpLoc OpVal
+    | Lea OpLoc OpVal
+
+    | Push OpVal
+    | Pop OpLoc
+
+    | Add OpLoc OpVal OpVal
+    | Sub OpLoc OpVal OpVal
+    | Mul OpLoc OpVal OpVal
+    | Div OpLoc OpVal OpVal
+    | Mod OpLoc OpVal OpVal
+
+    | Incr OpLoc
+    | Decr OpLoc
+    
+    | Equal        OpLoc OpVal OpVal 
+    | Less         OpLoc OpVal OpVal 
+    | Greater      OpLoc OpVal OpVal 
+    | LessEqual    OpLoc OpVal OpVal 
+    | GreaterEqual OpLoc OpVal OpVal
+
+    | Not OpLoc OpVal
+
+    | Jmp         InstructionIx
+    | JmpIf OpVal InstructionIx
+
+    | Call InstructionIx
+    | Ret
+
+    | SyscallExit 
+    | SyscallPrint
+
+    deriving (Eq, Show)
+
+
+
+data VM = VM {
+        codeMemory  :: [Op],
+        stackMemory :: [IntVal],
+        -- heapMemory  :: Map Address [IntVal],
+        specialRegisters :: SpecialRegisters,
+        generalRegisters :: GeneralRegisters
+    }
+
+
+step :: VM -> Running' VMRequest VM
+step vm@VM{codeMemory=ops, stackMemory=stack, specialRegisters=specs@SpecialRegisters{programCounter=(InstructionIx pc)}}
+    | pc >= length ops = Error $ "instruction pointer out of bounds"
+    | otherwise = let vm' = vm {specialRegisters=specs{programCounter=InstructionIx $ pc+1}} in
+        case ops !! pc of
+            Nop                   ->  pure vm'
+
+            Mov dst src           ->  do val <- getVal src vm
+                                         setLoc dst val $ vm'
+            Lea dst loc           ->  do addr <- getLoc loc vm
+                                         setLoc dst addr $ vm'
+
+            Push v                ->  push v vm'
+            Pop  dst              ->  pop dst vm'
+
+            Add dst a b           ->  binop (+) dst a b vm'
+            Sub dst a b           ->  binop (-) dst a b vm'
+            Mul dst a b           ->  binop (*) dst a b vm'
+            Div dst a b           ->  binop (div) dst a b vm'
+            Mod dst a b           ->  binop (mod) dst a b vm'
+
+            Incr loc              ->  incrop (+ 1)        loc vm'
+            Decr loc              ->  incrop (subtract 1) loc vm'
+            
+            Equal        dst a b  ->  binop' (==) dst a b vm' 
+            Less         dst a b  ->  binop' (<)  dst a b vm'
+            Greater      dst a b  ->  binop' (>)  dst a b vm' 
+            LessEqual    dst a b  ->  binop' (<=) dst a b vm' 
+            GreaterEqual dst a b  ->  binop' (>=) dst a b vm'
+
+            Not dst x             ->  unop' (not) dst x vm'
+
+            Jmp     loc           ->  jmp loc vm
+            JmpIf x loc           ->  do xv <- getVal x vm
+                                         if (intToBool xv) then jmp loc vm else pure vm'
+
+            Call loc              ->  do vm2 <- push (OpValConst Val $ pc+1) vm
+                                         jmp loc vm2
+            Ret                   ->  do (retLoc, vm2) <- pop' vm
+                                         setLoc (OpLocReg Val . SpecialRegister $ ProgramCounter) retLoc vm2
+            SyscallExit           ->  do (exitVal, _) <- pop' vm
+                                         Stop $ show exitVal
+            SyscallPrint          ->  do (ptr, vm2) <- pop' vm'
+                                         (len, vm3) <- pop' vm2
+                                         let str = map chr . take len . drop ptr . stackMemory $ vm3
+                                         if (length str) /= len
+                                            then Error "bad print syscall"
+                                            else Request (Print str (\_ -> pure vm3))
+        where
+            binop' f = binop (\a b -> boolToInt $ f a b)
+            binop f dst a b vm = do
+                av <- getVal a vm
+                bv <- getVal b vm
+                let res = {-trace' ((show av) ++ " `op` " ++ (show bv) ++ " -> ")-} (av `f` bv)
+                setLoc dst res $ vm
+            incrop f loc vm = do 
+                v <- getVal (toVal loc) vm
+                setLoc loc (f v) $ vm
+            unop' f = unop (boolToInt . f . intToBool)
+            unop f dst x vm = do
+                v <- getVal x vm
+                setLoc dst (f v) $ vm
+            jmp loc vm = pure $ vm {specialRegisters=(specialRegisters vm){programCounter=loc}}
+            push v vm = do
+                val <- getVal v vm
+                let stackTopIx' = 1 + (stackTop . specialRegisters $ vm)
+                stack' <- (fromMaybe $ "bad stackTop: " ++ (show stackTopIx')) $ setAt' stackTopIx' val (stackMemory vm)
+                pure $ vm {stackMemory = stack', specialRegisters = (specialRegisters vm) {stackTop=stackTopIx'}}
+            pop dst vm = do
+                (val, vm2) <- pop' vm
+                setLoc dst val vm2
+            pop' vm = do
+                let stackTopIx = stackTop (specialRegisters vm)
+                val <- (fromMaybe $ "bad stackTop: " ++ (show stackTopIx)) $ (stackMemory vm) !? stackTopIx
+                let vm2 = vm {specialRegisters=(specialRegisters vm){stackTop=stackTopIx-1}}
+                pure (val, vm2)
+
+
+            getVal :: (Functor r) => OpVal -> VM -> Running' r IntVal
+            getVal (OpValConst Val n) vm = pure n
+            getVal (OpValReg   Val (GeneralRegister (GeneralRegisterId i))) vm = (fromMaybe "invalid register") . (!? i) . generalRegisters $ vm
+            getVal (OpValReg   Val (SpecialRegister r)) vm = pure . getR . specialRegisters $ vm where
+                getR :: SpecialRegisters -> IntVal
+                getR = case r of
+                        ProgramCounter -> coerce . programCounter
+                        StackBase -> stackBase
+                        StackTop -> stackTop
+            getVal (OpValConst (Ref offset) addr) vm = (fromMaybe  "invalid stack address") . (!? (addr + offset)) . stackMemory $ vm
+            getVal (OpValReg   (Ref offset) r   ) vm = do addr <- getVal (OpValReg Val r) vm; getVal (OpValConst (Ref offset) addr) vm
+
+
+            setLoc :: (Functor r) => OpLoc -> IntVal -> VM -> Running' r VM 
+            setLoc (OpLocReg Val (GeneralRegister (GeneralRegisterId i))) val vm = do
+                grs <- (fromMaybe "invalid register") . (setAt' i val) . generalRegisters $ vm
+                pure $ vm {generalRegisters=grs}
+            setLoc (OpLocReg Val (SpecialRegister r)) val vm =
+                let srs = setR val . specialRegisters $ vm
+                 in pure $ vm {specialRegisters=srs} where
+                setR val srs = case r of
+                                ProgramCounter -> srs {programCounter=InstructionIx val}
+                                StackBase -> srs {stackBase=val}
+                                StackTop -> srs {stackTop=val}
+            setLoc (OpLocReg (Ref offset) r) val vm = do
+                addr <- getVal (OpValReg Val r) vm
+                setLoc (OpLocAddr (addr + offset)) val vm
+            setLoc (OpLocAddr addr) val vm = do
+                smem <- (fromMaybe  "invalid stack address") . (setAt' addr val) . stackMemory $ vm
+                pure $ vm {stackMemory=smem}
+            -- setLoc (OpLocAddr (HeapAddress addr))  val vm = undefined
+            -- setLoc (OpLocAddr (HeapAddress addr))  val vm = do hmem <- (fromMaybe  "invalid heap address")  . (M.insert addr val) . heapMemory $ vm; pure vm {heapMemory=hmem}
+
+            getLoc :: (Functor r) => OpVal -> VM -> Running' r IntVal
+            getLoc (OpValConst (Ref offset) addr) vm = pure (addr + offset)
+            getLoc (OpValReg   (Ref offset) r   ) vm = do addr <- getVal (OpValReg Val r) vm; getLoc (OpValConst (Ref offset) addr) vm
+            getLoc x _ = Error $ "invalid operand for LEA: " ++ (show x)
+
+
+            fromMaybe :: (Functor r) => String -> Maybe a -> Running' r a
+            fromMaybe msg Nothing = Error msg
+            fromMaybe _   (Just a) = Next a
+
+            takeLast n xs = drop ((length xs) - n) xs
+            dropLast n xs = take ((length xs) - n) xs
+
+            toVal :: OpLoc -> OpVal
+            toVal (OpLocAddr addr) = OpValConst (Ref 0) addr
+            toVal (OpLocReg r reg)  = OpValReg r reg
+
+            safeLast [] = Nothing
+            safeLast xs = Just $ last xs
+
+            safeInit [] = Nothing
+            safeInit xs = Just $ init xs
+
+            setAt' :: Int -> a -> [a] -> Maybe [a]
+            setAt' i x xs
+                | i < 0 || i >= (length xs) = Nothing
+                | otherwise = Just $ setAt i x xs
+
+
+            snoc xs x = xs ++ [x]
+
+(!?) :: [a] -> Int -> Maybe a
+xs !? i 
+    | (i >= 0) && (i < length xs) = Just $ xs !! i
+    | otherwise = Nothing 
+
+
+trace' s x = trace (s ++ " " ++ (show x)) x 
+
+
+
+prettyShowVM :: VM -> String
+prettyShowVM VM {codeMemory=ops, stackMemory=stack, generalRegisters = regs, specialRegisters = specs @ SpecialRegisters {programCounter = InstructionIx pc, stackTop = stackTopIx, stackBase = stackBaseIx}} =
+    unlines $ [specs', separate "|" regs' stack', "-------"] ++ ops'
+    where
+        specs' = "ESP:" ++ (show stackTopIx) ++ " " ++ "EBP:" ++ (show stackBaseIx)
+        regs' = (joinWith " " regs)
+        stack' = joinWith " " $ take (stackTopIx+1) stack
+        -- stack' = joinWith " " $ stack
+        ops' = catMaybes . map (\i -> showOp i <$> (ops !? i)) $ [pc, pc+1, pc+2]
+        showOp i op = (show i) ++ "  " ++ (show op) ++ (if i == pc then "  <--- " else "")
+
+
+separate sep s1 s2 = s1' ++ sep ++ s2'
+    where
+        s1' = if null s1 then s1 else s1 ++ " "
+        s2' = if null s2 then s2 else " " ++ s2
+
+joinWith sep = concat . intersperse sep . map show
+
+
+execProgram :: [Op] -> VM
+execProgram ops = VM { codeMemory = ops
+                     , stackMemory = replicate 40 0
+                     , generalRegisters = replicate 10 0
+                     , specialRegisters = SpecialRegisters {programCounter = InstructionIx 0
+                                                           , stackBase = -1
+                                                           , stackTop = -1}}
+
+iterVM :: VM -> [Either String VM]
+iterVM vm = (Right vm) : (iterVM' $ step vm) where
+
+    iterVM' (Error e  ) = [Left $ "Error: "    ++ e]
+    iterVM' (Stop  r  ) = [Left $ "Stopped. "  ++ r]
+    iterVM' (Next  vm') = iterVM vm'
+    iterVM' (Request f) = (Left . highlight $ "Request: " ++ (show f)) : rest where
+        rest = case f of
+                Read  _ k  -> iterVM' (k exampleInput)
+                Print s k  -> iterVM' (k ())
+                Exit  _ _  -> []
+    exampleInput = "hello"
+    highlight s = unlines $ [stars, s, stars] where stars = replicate 40 '#'
+
+
+esp       = OpLocReg Val       (SpecialRegister StackTop)
+espv      = OpValReg Val       (SpecialRegister StackTop)
+ebp       = OpLocReg Val       (SpecialRegister StackBase)
+ebpv      = OpValReg Val       (SpecialRegister StackBase)
+_ebp off  = OpLocReg (Ref off) (SpecialRegister StackBase)
+_ebpv off = OpValReg (Ref off) (SpecialRegister StackBase)
+_espv off = OpValReg (Ref off) (SpecialRegister StackTop)
+reg  i    = OpLocReg Val       (GeneralRegister . GeneralRegisterId $ i)
+regv i    = OpValReg Val       (GeneralRegister . GeneralRegisterId $ i)
+_reg  i   = OpLocReg (Ref 0)   (GeneralRegister . GeneralRegisterId $ i)
+_regv i   = OpValReg (Ref 0)   (GeneralRegister . GeneralRegisterId $ i)
+int n    = OpValConst Val n
+
+
+
+type Procedure = (String -> InstructionIx) -> [(String, [Op])]
+type Program = [Procedure]
+
+resolveLabels :: Program -> [Op]
+resolveLabels procs =
+    let dummyProg = concat . map ($ dummy) $ procs
+        labelOffsets = M.fromList . snd  .  mapAccumL (\off (lbl, len) -> (off+len, (lbl, off))) 0  .  map (second length) $ dummyProg
+    in concat . map snd . concat . map ($ (InstructionIx . (labelOffsets M.!))) $ procs
+    where
+        dummy = const (InstructionIx (-1))
+
+
+p0 :: Program
+p0 = [bootstrap, ptostr, parrrev, parrsum, pmain]
+
+bootstrap lbl = [
+    ("_bootstrap", [
+        Push ebpv,
+        Mov ebp espv,
+        Call (lbl "main"),
+        Mov esp ebpv,
+        Pop ebp,
+        Push (regv 0),
+        SyscallExit ])
+    ]
+    where
+
+
+pmain lbl = [
+    ("main", [
+        Nop,
+        Push (int (-10000)),
+        Push (int 0),
+        Push (int 0),
+        Push (int 0),
+        Push (int 0),
+        Push (int 0),
+        Push (int (-10000)),
+        Push (int 8000),
+        Push (int 40),
+        Push (int 20),
+        Push (int 10),
+        Push (int (-10000)),
+        Lea (reg rtemp) (_ebpv $ 2+7),
+        Push ebpv,
+        Mov ebp espv,
+        Push (int 4),     -- len
+        Push (regv rtemp), -- ptr
+        Call (lbl "arrsum"),
+        Mov esp ebpv,
+        Pop ebp,
+        Lea (reg rtemp) (_ebpv $ 2+1),
+        Push (regv rtemp),
+        Push ebpv,
+        Mov ebp espv,
+        Push (regv rtemp),  -- str
+        Push (regv 0),      -- num
+        Call (lbl "tostr"),
+        Mov esp ebpv,
+        Pop ebp,
+        Pop (reg rtemp),
+        Push (regv 0),
+        Push (regv rtemp),
+        SyscallPrint,
+        Push (_ebpv 1),
+        Ret ])
+    ]
+    where
+        rsum  = 0
+        rtemp = 1
+
+parrrev lbl = [
+    ("arrrev", [
+        Mov (reg rdst) (_ebpv 2),
+        Mov (reg rlen) (_ebpv 1),
+        Add (reg rdstback) (regv rdst) (regv rlen),
+        Decr (reg rdstback) ]),
+    ("arrev_loop", [
+        Less (reg rtemp) (regv rdst) (regv rdstback),
+        Not (reg rtemp) (regv rtemp),
+        JmpIf (regv rtemp) (lbl "arrev_end"),
+        Mov (reg rtemp) (_regv rdstback),
+        Mov (_reg rdstback) (_regv rdst),
+        Mov (_reg rdst) (regv rtemp),
+        Incr (reg rdst),
+        Decr (reg rdstback),
+        Jmp (lbl "arrev_loop") ]),
+    ("arrev_end", [
+        Ret ])
+    ]
+    where
+        rdst     = 0
+        rlen     = 1
+        rdstback = 2
+        rtemp    = 3
+
+ptostr lbl = [
+    ("tostr", [
+        Mov (reg rnum) (_ebpv 2),
+        Mov (reg rdst) (_ebpv 1),
+        Mov (reg rlen) (int 0),
+        Greater (reg rtemp) (regv rnum) (int 0),
+        JmpIf (regv rtemp) (lbl "tostr_loop"),
+        Mov (_reg rdst) (int 48),
+        Mov (reg 0) (int 1),
+        Ret ]),
+    ("tostr_loop", [
+        LessEqual (reg rtemp) (regv rnum) (int 0),
+        JmpIf (regv rtemp) (lbl "tostr_end"),
+        Mod (reg rtemp) (regv rnum) (int 10),
+        Add (reg rtemp) (regv rtemp) (int 48),
+        Mov (_reg rdst) (regv rtemp),
+        Div (reg rnum) (regv rnum) (int 10),
+        Incr (reg rdst),
+        Incr (reg rlen),
+        Jmp (lbl "tostr_loop") ]),
+    ("tostr_end", [
+        Sub (reg rdst) (regv rdst) (regv rlen),
+        Push (regv rlen),
+        Push ebpv,
+        Mov ebp espv,
+        Push (regv rlen),
+        Push (regv rdst),
+        Call (lbl "arrrev"),
+        Mov esp ebpv,
+        Pop ebp,
+        Pop (reg rlen),
+        Mov (reg 0) (regv rlen), 
+        Ret ])
+    ]
+    where
+        rnum     = 0
+        rlen     = 1
+        rdst     = 2
+        rtemp    = 3
+        rdstback = 4
+
+parrsum lbl = [
+    ("arrsum", [
+        Mov (reg rptr) (_ebpv 2),
+        Mov (reg rlen) (_ebpv 1),
+        Mov (reg rres) (int 0),
+        Mov (reg ri)   (int 0) ]),
+    ("arrsum_loop", [
+        Less (reg rtemp) (regv ri) (regv rlen),
+        Not (reg rtemp) (regv rtemp),
+        JmpIf (regv rtemp) (lbl "arrsum_end"),
+        Add (reg rres) (regv rres) (_regv rptr),
+        Incr (reg ri),
+        Incr (reg rptr),
+        Jmp (lbl "arrsum_loop") ]),
+    ("arrsum_end", [
+        Mov (reg 0) (regv rres),
+        Ret ])
+    ]
+     where
+        rlen  = 0
+        rptr  = 1
+        rres  = 2
+        ri    = 3
+        rtemp = 4
+
+
+
+
+main = do
+    -- let DDef funId args body = defAdd1 in
+    --     print $ evalCompile $ compileDef funId args body
+    case (Right . resolveLabels $ p0) :: Either String [Op] of
+
+        Left msg -> do
+            print msg
+
+        Right prog -> do
+            printCode prog
+            blank
+            blank
+            let vm = execProgram prog
+            forM_ (iterVM vm) $ either putStrLn (\x -> putStrLn (prettyShowVM x) >> blank)
+
+    where
+        print' x = print x >> blank 
+        blank = putStrLn ""
+        printCode = mapM putStrLn . map (uncurry showLine) . zip [0..]
+        showLine n c = show n ++ "\t" ++ show c
---
+++ StackVM.hs
@@ -0,0 +1,196 @@
+{-# LANGUAGE DeriveFunctor #-}
+-- {-# LANGUAGE NamedFieldPuns #-}
+-- {-# LANGUAGE RecordWildcards #-}
+-- {-# OPTIONS_GHC -Wall #-}
+module StackVM where
+
+import Data.Map (Map)
+import qualified Data.Map as M
+
+import Data.Void
+import Control.Monad (ap)
+
+-- ##################
+-- #       VM       #
+-- ##################
+
+
+type InstructionIx = Int
+type InstructionOffset = Int
+type StackIx = Int
+type VarIx = Int
+type IntVal = Int
+type ProcId = String
+
+
+data VM = VM {
+        procedures :: Map ProcId Proc,
+        frames :: [VMFrame]
+    } deriving (Eq, Show)
+
+data VMFrame = VMFrame {
+        instructionPointer :: InstructionIx,
+        instructions :: [Op],
+        vars  :: [IntVal],
+        stack :: [IntVal]
+    } deriving (Eq, Show)
+
+data Proc = Proc {
+        nArgs :: Int,
+        nVars :: Int,
+        code :: [Op]
+    } deriving (Eq, Show)
+
+
+
+
+empty = VM {
+    procedures = M.empty,
+    frames = []
+}
+
+emptyVM = empty
+
+emptyFrame = VMFrame {
+    instructionPointer = 0,
+    instructions = [],
+    stack = [],
+    vars = []
+}
+
+data Program = Program {mainProc :: Proc, allProcs :: Map ProcId Proc}
+
+execProgram :: Program -> VM
+execProgram p = emptyVM { frames = [procFrame (mainProc p)] , procedures=allProcs p}
+
+
+procFrame :: Proc -> VMFrame
+procFrame proc = emptyFrame { instructions=(code proc),
+                              vars=(replicate (nVars proc) 0) }
+
+
+data Op
+    = Nop
+    | Push IntVal | Pop | Dup
+    -- | Get StackIx | Set StackIx
+    | Load VarIx | Store VarIx
+    | Add | Mul | Sub | Incr | Decr
+    | Equal | Less | Greater | LessEqual | GreaterEqual
+    | Not
+    | Jmp   InstructionIx  | JmpRel   InstructionOffset
+    | JmpIf InstructionIx  | JmpRelIf InstructionOffset
+    | Call ProcId Int | Ret
+    deriving (Eq, Show)
+
+
+
+data Running e r f a
+    = Error e
+    | Stop r
+    | Next a
+    | Request (f (Running e r f a))
+    deriving Functor
+
+instance (Functor f) => Applicative (Running e r f) where
+    pure = Next
+    (<*>) = ap
+    -- (Next f) <*> (Next a)     = Next $ f a
+    -- (Next f)     <*> (Request ra) = Request $ fmap (fmap f) ra
+    -- (Request rf) <*> (Next a)     = Request $ fmap (fmap ($ a)) rf
+    -- (Request rf) <*> (Request ra) = Request $ rf <*> ra
+        -- rf :: VMR (Running e r VMR (a -> b))
+        -- ra :: VMR (Running e r VMR a)
+    -- (Error e) <*> _ = Error e
+    -- (Stop r) <*> _ = Stop r
+
+instance (Functor f) => Monad (Running e r f) where
+    return = pure
+    (Next a) >>= k = k a
+    (Request ra) >>= k = Request $ fmap (>>= k) ra
+        -- ra :: VMR a
+        -- k  :: a -> Request e r VMR b
+    (Error e) >>= _ = Error e
+    (Stop r)  >>= _ = Stop r
+
+
+data VMRequest a
+    = Print String (()     -> a)
+    | Read  ()     (String -> a)
+    | Exit  ()     (Void   -> a)
+    deriving Functor
+
+instance Show (VMRequest a) where
+    show (Print s _) = "Print " ++ (show s) ++ " (_ :: () -> a)"
+    show (Read  _ _) = "Read () (_ :: String -> a)"
+    show (Exit  _ _) = "Exit () (_ :: Void -> a)"
+
+type Running' f a = Running String String f a
+
+
+step :: VM -> Running' VMRequest VM 
+step (VM {frames=[]}) =  Stop "No frames remaining, halting"
+step vm@(VM {frames=frame@(VMFrame {instructionPointer=ip, instructions=ops, stack=st, vars=vs}) : outerFrames, procedures=procs})
+    | ip >= length ops = Error $ "instruction pointer out of bounds, stack: " ++ (show st)
+    | otherwise = (\fr -> vm {frames = fr}) <$> frames'
+       where
+        frame' = frame {instructionPointer = ip+1}
+        frames' = case (ops !! ip, st) of 
+            (Nop,           _           ) -> Next $ frame' : outerFrames
+            (Push val,      stack'      ) -> Next $ frame' {stack = val : stack'}     : outerFrames
+            (Pop,           (_:stack')  ) -> Next $ frame' {stack = stack'}           : outerFrames
+            (Dup,           (x:stack')  ) -> Next $ frame' {stack = (x : x : stack')} : outerFrames
+            -- (Get ix,        (stack')    ) -> Next $ frame' {stack = (stack' !! ix) : stack'} : outerFrames
+            -- (Set ix,        (x:stack')  ) -> Next $ frame' {stack = setAt (ix-1) x stack'} : outerFrames
+            (Load  ix,      stack'      ) -> Next $ frame' {stack = ((vs !! ix) : stack')}        : outerFrames
+            (Store ix,      (x:stack')  ) -> Next $ frame' {stack = stack', vars=setAt (ix) x vs} : outerFrames
+            (Add,           (a:b:stack')) -> Next $ frame' {stack = a+b : stack'} : outerFrames
+            (Sub,           (a:b:stack')) -> Next $ frame' {stack = b-a : stack'} : outerFrames
+            (Mul,           (a:b:stack')) -> Next $ frame' {stack = a*b : stack'} : outerFrames
+            (Incr,          (x:stack')  ) -> Next $ frame' {stack = x+1 : stack'} : outerFrames
+            (Decr,          (x:stack')  ) -> Next $ frame' {stack = x-1 : stack'} : outerFrames
+            (Equal,         (a:b:stack')) -> Next $ frame' {stack = (boolToInt $ a==b) : stack'} : outerFrames
+            (Less,          (a:b:stack')) -> Next $ frame' {stack = (boolToInt $ b < a) : stack'} : outerFrames
+            (Greater,       (a:b:stack')) -> Next $ frame' {stack = (boolToInt $ b > a) : stack'} : outerFrames
+            (LessEqual,     (a:b:stack')) -> Next $ frame' {stack = (boolToInt $ b <= a) : stack'} : outerFrames
+            (GreaterEqual,  (a:b:stack')) -> Next $ frame' {stack = (boolToInt $ b >= a) : stack'} : outerFrames
+            (Not,           (b:stack')  ) -> Next $ frame' {stack = (boolToInt . not . intToBool $ b) : stack'} : outerFrames
+            (Jmp ip',       _           ) -> Next $ frame' {instructionPointer=ip'}    : outerFrames
+            (JmpRel off,    _           ) -> Next $ frame' {instructionPointer=ip+off} : outerFrames
+
+            (JmpIf ip',     (c:stack')  ) -> Next $ if intToBool c
+                                                       then frame' {instructionPointer=ip',    stack=stack'} : outerFrames
+                                                       else frame' {stack=stack'} : outerFrames
+
+            (JmpRelIf off,  (c:stack')  ) -> Next $ if intToBool c
+                                                       then frame' {instructionPointer=ip+off, stack=stack'} : outerFrames
+                                                       else frame' {stack=stack'} : outerFrames
+            (Call procId nArgsPassed, stack')
+                | length stack' < nArgsPassed -> Error $ (
+                    "Not enough arguments to call " ++ (show procId) ++
+                    " with stack " ++ (show stack') ++
+                    "(expected" ++ (show nArgsPassed) ++ ")")
+                | otherwise -> case M.lookup procId procs of
+                    Nothing   -> Error $ "Undefined procedure " ++ (show procId)
+                    Just proc@Proc{nArgs=nArgs}
+                        | nArgsPassed /= nArgs -> Error $
+                            "Arity mismatch: " ++ (show procId) ++ " requires " ++ (show nArgs) ++" arguments, " ++
+                            " but was called with " ++ (show nArgsPassed)
+                        | otherwise -> Next $ functionFrame {stack = args} : frame {stack = stack''} : outerFrames
+                            where functionFrame = (procFrame proc)
+                                  (args, stack'') = splitAt nArgsPassed stack' 
+
+            (Ret, (x:stack')) -> case outerFrames of
+                [] -> Stop $ "main returned " ++ (show x)
+                (outer@VMFrame{stack=outerStack, instructionPointer=outerIp} : outerFrames') ->
+                    Next $ outer {stack = x:outerStack, instructionPointer=outerIp+1} : outerFrames'
+
+            (op, stack') -> Error $
+                "Cannot execute " ++ (show op) ++ " with stack " ++ (show stack')
+
+intToBool :: Int -> Bool
+intToBool = (/= 0)
+boolToInt :: Bool -> Int
+boolToInt x = if x then 1 else 0 
+
+setAt :: Int -> a -> [a] -> [a]
+setAt i x =  map (\(j,y) -> if j==i then x else y) . zip [0..]
